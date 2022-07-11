c642e9ed52bad5eb9a41ccb1c91ffba489efcf00
dongnt <dongnt@newwave.vn>
2022-07-11 10:05:08 +0700

    add handle arrow in batte zone

--- assets/Scripts/CardTouchEvent.ts
+++ assets/Scripts/CardTouchEvent.ts
@@ -7,15 +7,21 @@
 
 import HandZone from './HandZone';
 
-const { ccclass } = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class CardTouchEvent extends cc.Component {
 
+  @property(cc.Prefab)
+  chooseArrow: cc.Prefab = null
+  currentArrow: cc.Node = null
+
+  @property(cc.Prefab)
+  cardBorderGlow: cc.Prefab = null
+
   currentCardAtr: CardAtribute = new CardAtribute()
   handZone: HandZone = null;
   isEnterBatteZone: boolean = false;
-
   manager = cc.director.getCollisionManager();
 
   onLoad() {
@@ -29,20 +35,15 @@ export default class CardTouchEvent extends cc.Component {
     }
   }
 
-  // onCollisionStay(otherCollider: cc.BoxCollider, selfCollider: cc.BoxCollider) {
-  //   console.log("stay")
-  //   if (selfCollider.node.name === this.node.name && otherCollider.node.name === "BattleZone") {
-  //     this.isEnterBatteZone = true
-  //   }
-  // }
-
   onCollisionExit(otherCollider: cc.BoxCollider, selfCollider: cc.BoxCollider) {
     this.isEnterBatteZone = false
   }
 
-  scaleUp(event: cc.Event.EventMouse) {
-    this.manager.enabledDebugDraw = true;
+  scaleUp(event: cc.Event.EventTouch) {
+    //enable collider
     this.manager.enabled = true;
+
+    //scale card up
     const node = event.currentTarget
     this.currentCardAtr.angle = node.angle
     this.currentCardAtr.x = node.x
@@ -52,17 +53,29 @@ export default class CardTouchEvent extends cc.Component {
       .to(0.05, { y: this.currentCardAtr.y <= 0 ? 40 : node.y, scale: 1, zIndex: 100, angle: 0 }, { easing: 'quadInOut' })
       .start();
     this.node.on(cc.Node.EventType.TOUCH_END, this.scaleDown, this)
+    this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.scaleDown, this)
     this.node.on(cc.Node.EventType.TOUCH_MOVE, this.moveCard, this)
   }
 
   scaleDown() {
     if (this.isEnterBatteZone) {
-      this.manager.enabledDebugDraw = false;
+      //disanable collider
       this.manager.enabled = false;
+
+      //move card to battle zone
       this.handZone.goToBattleZone(this.node)
+
+      //turn off touch event
       this.node.off(cc.Node.EventType.TOUCH_START)
       this.node.off(cc.Node.EventType.TOUCH_END)
+      this.node.off(cc.Node.EventType.TOUCH_CANCEL)
       this.node.off(cc.Node.EventType.TOUCH_MOVE)
+
+      //new event for card in battle zonesdasdaasd
+      this.node.on(cc.Node.EventType.TOUCH_START, this.showArrow, this)
+      this.node.on(cc.Node.EventType.TOUCH_MOVE, this.controlArrow, this)
+      this.node.on(cc.Node.EventType.TOUCH_END, this.removeArrow, this)
+      this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.removeArrow, this)
     } else {
       this.handZone.reSoftCard()
     }
@@ -72,7 +85,56 @@ export default class CardTouchEvent extends cc.Component {
 
   }
 
-  moveCard(event: cc.Event.EventMouse) {
+  showArrow(event: cc.Event.EventTouch) {
+    //add card glow
+    const cardGlow = cc.instantiate(this.cardBorderGlow)
+    cardGlow.setPosition(0, 0)
+    this.node.addChild(cardGlow)
+
+    //add arrow
+    const arrow = cc.instantiate(this.chooseArrow)
+    const nodeSpace = (this.node.convertTouchToNodeSpaceAR(event.touch))
+    arrow.setPosition(nodeSpace.x, nodeSpace.y)
+    arrow.zIndex = -1
+    this.currentArrow = arrow
+    this.node.addChild(arrow)
+  }
+
+  removeArrow(event: cc.Event.EventTouch) {
+    //remove card glow
+    this.node.getChildByName("CardBorderGlow")?.destroy()
+
+    //remove card glow
+    this.currentArrow.destroy()
+  }
+
+  controlArrow(event: cc.Event.EventTouch) {
+    //get current x,y
+    const nodeSpace = (this.node.convertTouchToNodeSpaceAR(event.touch))
+    let x = nodeSpace.x
+    let y = nodeSpace.y
+
+    //set caculate and set angle for arrow
+    const firstAngle = Math.atan2(1, 0);
+    const secondAngle = Math.atan2(y, x);
+    let angle = secondAngle - firstAngle;
+    angle = angle * 180 / Math.PI;
+    this.currentArrow.angle = angle
+
+    //set posotion for arrow
+    const isOutOfHeight = Math.pow(this.currentArrow.height, 2) <= Math.pow(Math.abs(x), 2) + Math.pow(Math.abs(y), 2)
+
+    if (isOutOfHeight) {
+      const xm = nodeSpace.x < 0 ? -1 : 1
+      const ym = nodeSpace.y < 0 ? -1 : 1
+
+      x = xm * Math.abs(Math.sin(angle * Math.PI / 180) * this.currentArrow.height);
+      y = ym * Math.abs(Math.cos(angle * Math.PI / 180) * this.currentArrow.height);
+    }
+    this.currentArrow.setPosition(x, y)
+  }
+
+  moveCard(event: cc.Event.EventTouch) {
     const x = event.currentTarget.x + event.getDeltaX()
     const y = event.currentTarget.y + event.getDeltaY()
     this.currentCardAtr.angle = 0
